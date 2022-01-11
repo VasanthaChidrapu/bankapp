5a07e8286ce60224a5429de73a13eb1a78148fc6
VASANTHA <chidrapu.vasantha@capgemini.com>
2022-01-11 14:11:07 +0530

    get added

--- src/main/java/com/springboot/bankapp/service/CustomerService.java
+++ src/main/java/com/springboot/bankapp/service/CustomerService.java
@@ -14,6 +14,7 @@ import com.springboot.bankapp.model.Role;
 import com.springboot.bankapp.model.UserInfo;
 import com.springboot.bankapp.repository.CustomerRepository;
 import com.springboot.bankapp.repository.RoleRepository;
+import com.springboot.bankapp.repository.UserRepository;
 
 @Service
 public class CustomerService {
@@ -26,7 +27,8 @@ public class CustomerService {
 	
 	@Autowired
 	private RoleRepository roleRepository;
-	
+	@Autowired
+	private UserRepository userRepository; 
 	public Customer postCustomer(Customer customer) {
 		 //generate 10 digit account number
 		Random random = new Random();
@@ -59,6 +61,10 @@ public class CustomerService {
 	}
 	public Customer getCustomerById(Long id) {
 		return customerRepository.getById(id);
+	}
+public UserInfo getUserByName(String name) {
+		
+		return userRepository.findByUsername(name);
 	}
 	
 	
